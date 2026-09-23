09681b1a77838138de48a0dabfbe4b34e45775de
naitikkumarsingh <prk34ktraj@gmail.com>
2026-09-23 19:29:37 +0530

    Add files via upload

---
+++ My Python Project.py
@@ -0,0 +1,10 @@
+print("Hey, How old are you. Show me your id")
+User_age = 15
+print("Hi, I am 15. Here is my id")
+if User_age < 18: 
+    print("You are not allowed to drive and you are under juvenile detention.")
+elif User_age == 18:   
+        print("You are just eligible to drive.")
+else:
+    print("You are allowed to drive.")
+   
